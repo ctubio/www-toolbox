8f2939796e744011be583a6f9f638ef1c9889842
Carles Tubio <ctubio@users.noreply.github.com>
2015-07-04 01:19:13 +0000

    Updated configurable index.

--- src/ctubio/ItsToolTime/WWWToolbox.php
+++ src/ctubio/ItsToolTime/WWWToolbox.php
@@ -1,5 +1,6 @@
 <?php namespace ctubio\ItsToolTime;
 
+use ctubio\ItsToolTime\Controller\ErrorController;
 use Pux\Executor;
 use Pux\Mux;
 
@@ -15,16 +16,17 @@ class WWWToolbox {
 
   public function __construct($tools = array()) {
     $this->mux = new Mux();
-    $this->mux->mount('/error', new Controller\ErrorController());
+    $this->mux->mount('/error', new ErrorController());
 
     if (!$tools) $tools = $this->tools;
 
     if (is_string($tools)) $tools = array($tools);
 
     foreach($tools as $k => $v)
-      if (in_array($v, $this->tools))
-        $this->mux->mount('/'.(is_numeric($k)?$v:$k), new Controller\DummyController());
-      else throw new \Exception('Unknown tool: "'.$v.'".');
+      if (in_array($v, $this->tools)) {
+        $class = __NAMESPACE__.'\\Controller\\'.ucfirst($v).'Controller';
+        $this->mux->mount('/'.(is_numeric($k)?$v:$k), new $class());
+      } else throw new \Exception('Unknown tool: "'.$v.'".');
   }
 
   public function __toString() {
