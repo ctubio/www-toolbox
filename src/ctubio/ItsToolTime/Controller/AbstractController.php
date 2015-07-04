e10fb750fa6fdb94d501adfde95df35c69e435ff
Carles Tubio <ctubio@users.noreply.github.com>
2015-07-04 17:19:13 +0000

    Added distributed layout.

--- src/ctubio/ItsToolTime/Controller/AbstractController.php
+++ src/ctubio/ItsToolTime/Controller/AbstractController.php
@@ -6,8 +6,13 @@ use Pux\Controller;
 class AbstractController extends Controller {
   protected function wrapLayout($content) {
     $parser = new Parser();
-    return $parser->parse(file_get_contents('../skin/abstractLayout.lex'), [
-      'content' => $content
-    ]);
+    return $parser->parse(
+      file_get_contents('../skin/abstractLayout.lex'.(
+        file_exists('../skin/abstractLayout.lex')
+          ? NULL : '.dist'
+      )), [
+        'content' => $content
+      ]
+    );
   }
 }
