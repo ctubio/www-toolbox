ad99fe12623dcbb7b8bd0f8c574c66067315517f
Carles Tubio <ctubio@users.noreply.github.com>
2016-10-30 12:33:51 +0000

    Updated styles.

--- pub/www-toolbox.php
+++ pub/www-toolbox.php
@@ -6,7 +6,7 @@ ini_set('error_reporting', E_ALL);
 
 require '../vendor/autoload.php';
 
-define('WWWToolboxPathPrefix', '/tools');
+define('WWWToolboxPathPrefix', '/tools'); # public path url (href values prefix)
 
 echo new WWWToolbox(
   WWWToolbox::ALL_TOOLS
--- src/ctubio/ItsToolTime/Controller/AbstractController.php
+++ src/ctubio/ItsToolTime/Controller/AbstractController.php
@@ -14,6 +14,7 @@ class AbstractController extends Controller {
       ));
     return $this->parseLayout('index', [
       'tools' => $tools,
+      'path' => constant('WWWToolboxPathPrefix'),
     ]);
   }
 
